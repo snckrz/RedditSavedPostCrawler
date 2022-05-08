d7e3add72c9befc30e7f07104963d82e6ddd33a9
snckrz <josua.kollmann@gmail.com>
2022-05-08 22:40:46 +0200

    done

--- index.ts
+++ index.ts
@@ -1,20 +1,76 @@
-import fetch, { Request } from 'cross-fetch';
-// Or just: import 'cross-fetch/polyfill';
-let request = new Request('')
+const snoowrap = require('snoowrap');
+import https from 'https'; // or 'https' for https:// URLs
+import http from 'http'; // or 'https' for https:// URLs
+import fs from 'fs';
+
+require('dotenv').config()
+
+const config = {
+  username: process.env.username,
+  password: process.env.password,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+}
+
+const r = new snoowrap({
+  userAgent: 'Whatever',
+  clientId: config.clientId,
+  clientSecret: config.clientSecret,
+  username: config.username,
+  password: config.password,
+})
 
 
-(async () => {
-  try {
-    const res = await fetch('https://reddit.com/api');
-
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-
-    const user = await res.json();
-
-    console.log(user);
-  } catch (err) {
-    console.error(err);
+// delete file if it already exists
+try {
+  if (fs.existsSync('urlsArray.txt')) {
+    fs.unlinkSync('urlsArray.txt');
   }
-})();
+} catch(err) {
+  console.error(err)
+}
+
+console.log("Start fetching, this might take some time ⏳")
+r.getUser('snckrz').getSavedContent().fetchAll().map((post: { url: any; }) => post.url).then(
+  response => {
+      response.forEach((element, index) => crawler(element, response, index));
+  }
+);
+
+function crawler(element: any, response: any, index: any) {
+  console.log(element);
+  
+  // Create file and write URL to it
+  fs.writeFileSync('urlsArray.txt', element, { flag: 'a+' });
+  fs.writeFileSync('urlsArray.txt', '\n', { flag: 'a+' });
+  
+  // check if saved Post isnt deleted
+  if(!element) {
+    return
+  }
+  // Refactor URL, because reddit&imgur save filename at the end of URL
+  let filenameToSave = element.split("/").reverse()[0];
+  console.log(filenameToSave);
+  if( filenameToSave == "" || !(filenameToSave.includes('.'))){
+    // skip if URL does not contain file ending
+    return;
+  }
+  if(filenameToSave.length > 50){
+    filenameToSave = index;
+  }
+  const file = fs.createWriteStream(`images/${filenameToSave}`);
+  function requestCallback(response) {
+    response.pipe(file);
+
+    // after download completed close filestream
+    file.on("finish", () => {
+        file.close();
+        console.log(`✅ Completed download of ${filenameToSave}`); 
+    });
+  }
+  if(element.includes("https")){
+    const request = https.get(element, requestCallback);
+  } else {
+    const request = http.get(element, requestCallback);
+  }
+}
